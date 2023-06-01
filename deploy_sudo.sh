0c87b7137d6b880f0a57f5b59e8a62fbdbe99656
Rafael Palamim <palamimweb@gmail.com>
2023-06-01 02:22:46 -0300

    Adicionando deploy_sudo.sh

---
+++ deploy_sudo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Instalando as dependências do composer (o docker compose na linha 20 utiliza o sail para montar o container)
+sudo docker run --rm \
+    -u "$(id -u):$(id -g)" \
+    -v "$(pwd)/api:/var/www/html" \
+    -w /var/www/html \
+    laravelsail/php82-composer:latest \
+    composer install --ignore-platform-reqs
+
+# Estarei utilizando o arquivo .env.example para esse deploy, porem o correto é colocar essas variaveis de ambiente diretamente no servidor
+rm ./api/.env
+cp ./api/.env.example ./api/.env
+
+# Removendo o arquivo sqlite antes de criar outro (caso execute o comando deploy novamente)
+rm -f ./api/database/database.sqlite
+touch ./api/database/database.sqlite
+
+# Subir containers
+sudo docker compose up -d --build
+
+# Build da estrutura do laravel
+sudo docker exec -it api_laravel php artisan migrate:fresh
+sudo docker exec -it api_laravel php artisan db:seed
+
+# Build da estrutura do react
+sudo docker exec -it front_react yarn install
+sudo docker exec -it front_react yarn build
--- readme.md
+++ readme.md
@@ -10,17 +10,25 @@ Entre na pasta criada
 
 Talvez seja necessário colocar permissão de execução no script
 
-> chmod +x deploy.sh
+```bash
+chmod +x deploy.sh
+ou
+chmod +x deploy_sudo.sh
+```
 
-Execute o comando deploy.sh
+Execute o comando deploy_sudo.sh
 
-> bash deploy.sh
+```bash
+bash deploy.sh
+ou
+bash deploy_sudo.sh
+```
 
-O front-end será servido em http://localhost:3000
+O front-end será servido em http://localhost:3000. A API, em http://localhost
 
 # Área administrativa
 
 usuário: admin@verzel.com
 senha: password
 
-Esse acesso foi criado no processo de seed, crio também 10 veículos por padrão
+O acesso foi criado no processo de seed, crio também 10 veículos por padrão.
